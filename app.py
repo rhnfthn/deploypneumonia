afccd94c41c7fa9a9fcc25c630ef5ffa82cd335a
rhnfthn <raihanfathan11@gmail.com>
2025-07-15 19:47:33 +0700

    first commit

--- app.py
+++ app.py
@@ -68,4 +68,6 @@ def faq():
     return render_template('faq.html')
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port) 
